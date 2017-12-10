0ee245c83af4eba5851a80baff7b3d68b25bc75d
q1701 <q1701@outlook.com>
2017-12-10 16:54:45 +0900

    M+ MN Type-1 の 5 ウェイト用のシェルスクリプトを作成

---
+++ generate-latin-only-mplus-1mn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./generate_latin_only_fonts.py \
+    ./fonts_in/mplus-1mn-bold.ttf \
+    ./fonts_in/mplus-1mn-light.ttf \
+    ./fonts_in/mplus-1mn-medium.ttf \
+    ./fonts_in/mplus-1mn-regular.ttf \
+    ./fonts_in/mplus-1mn-thin.ttf \
+    ./fonts_out
